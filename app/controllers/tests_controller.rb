2c8e6b0830aa5dee97f6561bbff261b77a370ede
Bryan Paradis <bryan.paradis@shopify.com>
2018-05-09 15:08:48 -0700

    Feature: Import via url. Give screenshot a url instead of File

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,4 +1,6 @@
 require 'image_processor'
+require 'net/http'
+require 'tempfile'
 
 class TestsController < ApplicationController
   skip_before_action :verify_authenticity_token
@@ -17,9 +19,24 @@ class TestsController < ApplicationController
   end
 
   def create
-    ImageProcessor.crop(test_params[:screenshot].path, test_params[:crop_area]) if test_params[:crop_area]
-    @test = Test.create!(test_params)
-    ScreenshotComparison.new(@test, test_params[:screenshot])
+    screenshot = test_params[:screenshot]
+
+    if(screenshot.is_a?(String))
+       url = URI.parse(screenshot)
+       response = Net::HTTP.get_response(url)
+       
+       temp_file = Tempfile.new('test', :encoding => 'ascii-8bit')
+       temp_file.write(response.body)
+       
+       http_file = ActionDispatch::Http::UploadedFile.new(tempfile: temp_file)
+       http_file.content_type = response.content_type
+      
+       screenshot = http_file
+    end
+
+    ImageProcessor.crop(screenshot.path, test_params[:crop_area]) if test_params[:crop_area]
+    @test = Test.create!(test_params.merge(screenshot: screenshot))
+    ScreenshotComparison.new(@test, screenshot)
     render json: @test.to_json
   end
 
